5c673762fc1904078604354d151d1de16876b3ab
Edward White <edwardwhite1@outlook.com>
2018-04-19 02:56:24 -0500

    added streamReader to read scores from, if the file exist, otherwise the program can crash!

--- LiarDice_grpProjCS/frmWinners.Designer.cs
+++ LiarDice_grpProjCS/frmWinners.Designer.cs
@@ -28,81 +28,162 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.lblPlayer = new System.Windows.Forms.Label();
+            this.lblcpu1 = new System.Windows.Forms.Label();
+            this.lblcpu2 = new System.Windows.Forms.Label();
             this.btnClose = new System.Windows.Forms.Button();
+            this.lblcpu3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPlayerResult = new System.Windows.Forms.Label();
+            this.lblCpu2Result = new System.Windows.Forms.Label();
+            this.lblCpu1Result = new System.Windows.Forms.Label();
+            this.lblCpu3Result = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label1
+            // lblPlayer
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Broadway", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.Control;
-            this.label1.Location = new System.Drawing.Point(257, 130);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(457, 55);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Games You Won:";
+            this.lblPlayer.AutoSize = true;
+            this.lblPlayer.Font = new System.Drawing.Font("Broadway", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblPlayer.Location = new System.Drawing.Point(194, 152);
+            this.lblPlayer.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPlayer.Name = "lblPlayer";
+            this.lblPlayer.Size = new System.Drawing.Size(343, 41);
+            this.lblPlayer.TabIndex = 0;
+            this.lblPlayer.Text = "Games You Won:";
             // 
-            // label2
+            // lblcpu1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Broadway", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.Control;
-            this.label2.Location = new System.Drawing.Point(36, 287);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(678, 55);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Games Cpu player 1 won:";
+            this.lblcpu1.AutoSize = true;
+            this.lblcpu1.Font = new System.Drawing.Font("Broadway", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcpu1.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblcpu1.Location = new System.Drawing.Point(28, 249);
+            this.lblcpu1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblcpu1.Name = "lblcpu1";
+            this.lblcpu1.Size = new System.Drawing.Size(509, 41);
+            this.lblcpu1.TabIndex = 1;
+            this.lblcpu1.Text = "Games Cpu player 1 won:";
             // 
-            // label3
+            // lblcpu2
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Broadway", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.SystemColors.Control;
-            this.label3.Location = new System.Drawing.Point(36, 444);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(680, 55);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Games Cpu Player 2 won:";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Broadway", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.SystemColors.Control;
-            this.label4.Location = new System.Drawing.Point(36, 601);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(688, 55);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Games Cpu Player 3 Won:";
+            this.lblcpu2.AutoSize = true;
+            this.lblcpu2.Font = new System.Drawing.Font("Broadway", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcpu2.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblcpu2.Location = new System.Drawing.Point(28, 346);
+            this.lblcpu2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblcpu2.Name = "lblcpu2";
+            this.lblcpu2.Size = new System.Drawing.Size(510, 41);
+            this.lblcpu2.TabIndex = 2;
+            this.lblcpu2.Text = "Games Cpu Player 2 won:";
             // 
             // btnClose
             // 
             this.btnClose.Font = new System.Drawing.Font("Broadway", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClose.Location = new System.Drawing.Point(413, 724);
+            this.btnClose.Location = new System.Drawing.Point(310, 579);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(184, 73);
+            this.btnClose.Size = new System.Drawing.Size(138, 58);
             this.btnClose.TabIndex = 4;
             this.btnClose.Text = "D&ismiss";
             this.btnClose.UseVisualStyleBackColor = true;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
+            // lblcpu3
+            // 
+            this.lblcpu3.AutoSize = true;
+            this.lblcpu3.Font = new System.Drawing.Font("Broadway", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcpu3.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblcpu3.Location = new System.Drawing.Point(33, 443);
+            this.lblcpu3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblcpu3.Name = "lblcpu3";
+            this.lblcpu3.Size = new System.Drawing.Size(510, 41);
+            this.lblcpu3.TabIndex = 5;
+            this.lblcpu3.Text = "Games Cpu Player 3 won:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.NavajoWhite;
+            this.label1.Font = new System.Drawing.Font("Broadway", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(116, 53);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(560, 41);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Rounds Won by Each Player";
+            // 
+            // lblPlayerResult
+            // 
+            this.lblPlayerResult.AutoSize = true;
+            this.lblPlayerResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblPlayerResult.Font = new System.Drawing.Font("Broadway", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayerResult.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblPlayerResult.Location = new System.Drawing.Point(560, 152);
+            this.lblPlayerResult.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPlayerResult.Name = "lblPlayerResult";
+            this.lblPlayerResult.Size = new System.Drawing.Size(43, 43);
+            this.lblPlayerResult.TabIndex = 7;
+            this.lblPlayerResult.Text = "0";
+            // 
+            // lblCpu2Result
+            // 
+            this.lblCpu2Result.AutoSize = true;
+            this.lblCpu2Result.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCpu2Result.Font = new System.Drawing.Font("Broadway", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCpu2Result.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblCpu2Result.Location = new System.Drawing.Point(560, 346);
+            this.lblCpu2Result.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCpu2Result.Name = "lblCpu2Result";
+            this.lblCpu2Result.Size = new System.Drawing.Size(43, 43);
+            this.lblCpu2Result.TabIndex = 8;
+            this.lblCpu2Result.Text = "0";
+            // 
+            // lblCpu1Result
+            // 
+            this.lblCpu1Result.AutoSize = true;
+            this.lblCpu1Result.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCpu1Result.Font = new System.Drawing.Font("Broadway", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCpu1Result.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblCpu1Result.Location = new System.Drawing.Point(560, 249);
+            this.lblCpu1Result.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCpu1Result.Name = "lblCpu1Result";
+            this.lblCpu1Result.Size = new System.Drawing.Size(43, 43);
+            this.lblCpu1Result.TabIndex = 9;
+            this.lblCpu1Result.Text = "0";
+            // 
+            // lblCpu3Result
+            // 
+            this.lblCpu3Result.AutoSize = true;
+            this.lblCpu3Result.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCpu3Result.Font = new System.Drawing.Font("Broadway", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCpu3Result.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblCpu3Result.Location = new System.Drawing.Point(560, 443);
+            this.lblCpu3Result.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCpu3Result.Name = "lblCpu3Result";
+            this.lblCpu3Result.Size = new System.Drawing.Size(43, 43);
+            this.lblCpu3Result.TabIndex = 10;
+            this.lblCpu3Result.Text = "0";
+            // 
             // frmWinners
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Goldenrod;
-            this.ClientSize = new System.Drawing.Size(1053, 831);
-            this.Controls.Add(this.btnClose);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(790, 665);
+            this.Controls.Add(this.lblCpu3Result);
+            this.Controls.Add(this.lblCpu1Result);
+            this.Controls.Add(this.lblCpu2Result);
+            this.Controls.Add(this.lblPlayerResult);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblcpu3);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lblcpu2);
+            this.Controls.Add(this.lblcpu1);
+            this.Controls.Add(this.lblPlayer);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmWinners";
-            this.Text = "Wins From This Play Session";
+            this.Text = "Wins All Time";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -110,10 +191,15 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblPlayer;
+        private System.Windows.Forms.Label lblcpu1;
+        private System.Windows.Forms.Label lblcpu2;
         private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblcpu3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPlayerResult;
+        private System.Windows.Forms.Label lblCpu2Result;
+        private System.Windows.Forms.Label lblCpu1Result;
+        private System.Windows.Forms.Label lblCpu3Result;
     }
 }
--- LiarDice_grpProjCS/frmWinners.cs
+++ LiarDice_grpProjCS/frmWinners.cs
@@ -7,16 +7,73 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace LiarDice_grpProjCS
 {
+   
+
+
     public partial class frmWinners : Form
     {
+  
         public frmWinners()
         {
             InitializeComponent();
+            //call score function
+           ReturnScores();
         }
 
+        struct Scores
+        {
+            //really not needed to perform this task, 
+            // but using it for orgaization anyways. -Edward
+            public int player1Score;
+            public int cpuPlayer1Score;
+            public int cpuPlayer2Score;
+            public int cpuPlayer3Score;
+
+        }
+        private void ReturnScores()
+        {
+            Scores myScores = new Scores();
+            StreamReader fileIn;
+            // open the stream to look at the contents of a file
+            fileIn = File.OpenText("scores.txt");
+            //read each line for each score
+            int line = 0;
+            while(!fileIn.EndOfStream)
+            {
+                //depending on what line is the file reader is on, parse it to a certian item 
+                switch (line)
+                {
+                    case 1:
+                        myScores.player1Score = int.Parse(fileIn.ReadLine());
+                        line++;
+                       break;
+                    case 2:
+                        myScores.cpuPlayer1Score = int.Parse(fileIn.ReadLine());
+                        line++;
+                        break;
+                    case 3:
+                        myScores.cpuPlayer2Score = int.Parse(fileIn.ReadLine());
+                        line++;
+                        break;
+                    case 4:
+                        myScores.cpuPlayer3Score = int.Parse(fileIn.ReadLine());
+                        line++;
+                        break;
+                }
+            }
+            //close file
+            fileIn.Close();
+            // output each item to the form
+            lblPlayerResult.Text = myScores.player1Score.ToString();
+            lblCpu1Result.Text = myScores.cpuPlayer2Score.ToString();
+            lblCpu2Result.Text = myScores.cpuPlayer2Score.ToString();
+            lblCpu3Result.Text = myScores.cpuPlayer3Score.ToString();
+            
+        }
         private void btnClose_Click(object sender, EventArgs e)
         {
             //close this window
